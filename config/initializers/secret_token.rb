be7c27a18654af37af8d558e258cbec6494ccd40
David Padilla <david@crowdint.com>
2012-06-14 15:34:38 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoderetreatMx::Application.config.secret_token = 'ad8f24d04fa1e4c340a02a801029708c8fb0f52f4eb783bb98d530d652b953b3501c6291e7d309befc646b07fdcc82a9efb9cdf48508bc244f7212ebe5d8be75'
